7b8bd6f6a90e8b3f06cf90a780ca000c05c2ac34
Paulo Sousa <a89465@alunos.uminho.pt>
2020-03-05 17:32:00 +0000

    Mudanças no header

--- include/headers.h
+++ include/headers.h
@@ -4,24 +4,42 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Define
+
+#define MAX 64
+#define SMAX 10
+
 // Structs
 
-typedef struct sales{
-  char* prod;
-  float preco;
-  int uni;
-  char tipo;
-  char* cli;
-  int mes;
-  int filial;
-} Sales;
+typedef struct arr{
+  int used;
+  char** list;
+} ARR;
 
+typedef struct sale{
+  char* p;
+  float price;
+  int uni;
+  char* type;
+  char* c;
+  int month;
+  int branch;
+} SALE;
+
+typedef struct sales{
+  int used;
+  SALE* list;
+} SALES;
 
 // Functions
 
 void quickSort(char* arr[], int low, int high);
+int binarySearch(char** arr, char* code, int low, int high);
 
-void arrclientes (char** arr);
-void arrprodutos(char** arr);
+int salesBranch1(SALES* val);
 
-int salesToA();
+void arrclientes(ARR* cli);
+void arrprodutos(ARR* prod);
+
+void salesToA(SALES* val, SALES* nval, ARR* cli, ARR *prod);
+void salesToF(SALES* val);
